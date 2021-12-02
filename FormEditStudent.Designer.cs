058a70baf06b90804f10ac94249ae91f1fb7329a
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-12-02 14:44:10 -0500

    finished edit

--- FormEditStudent.Designer.cs
+++ FormEditStudent.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.lstStudents = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
             this.btnDone = new System.Windows.Forms.Button();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lstStudents
@@ -43,19 +46,20 @@
             this.lstStudents.Size = new System.Drawing.Size(178, 202);
             this.lstStudents.TabIndex = 0;
             // 
-            // label1
+            // lblInstructions
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(196, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(379, 36);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Select a student then enter the new name in the boxes \r\n(top first, bottom last) " +
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(196, 12);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(379, 36);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Select a student then enter the new name in the boxes \r\n(top first, bottom last) " +
     "then hit the button to change them.\r\n";
             // 
             // btnDone
             // 
+            this.btnDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDone.Location = new System.Drawing.Point(12, 220);
             this.btnDone.Name = "btnDone";
             this.btnDone.Size = new System.Drawing.Size(178, 27);
@@ -63,13 +67,43 @@
             this.btnDone.Text = "Done";
             this.btnDone.UseVisualStyleBackColor = true;
             // 
+            // txtFirst
+            // 
+            this.txtFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirst.Location = new System.Drawing.Point(199, 51);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(376, 24);
+            this.txtFirst.TabIndex = 3;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLast.Location = new System.Drawing.Point(199, 81);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(376, 24);
+            this.txtLast.TabIndex = 4;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(199, 111);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(121, 31);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FormEditStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 249);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtFirst);
             this.Controls.Add(this.btnDone);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.lstStudents);
             this.Name = "FormEditStudent";
             this.Text = "FormEditStudent";
@@ -81,7 +115,10 @@
         #endregion
 
         private System.Windows.Forms.ListBox lstStudents;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInstructions;
         private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
--- FormEditStudent.cs
+++ FormEditStudent.cs
@@ -16,5 +16,18 @@ namespace Part_10._5_Student_Forms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormStudents.students.RemoveAt(lstStudents.SelectedIndex);
+            if (txtFirst.Text.Trim() != "" && txtLast.Text.Trim() != "")
+            {
+                FormStudents.students.Insert(lstStudents.SelectedIndex,new Student(txtFirst.Text, txtLast.Text));
+                txtFirst.Text = "";
+                txtLast.Text = "";
+                lstStudents.DataSource = null;
+                lstStudents.DataSource = FormStudents.students;
+            }
+        }
     }
 }
